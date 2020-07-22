75158e2a467fbf3aa16cc802fda37a0d011afd75
Guilherme Sonnemann Eckert <guilhermeeckert@Guilhermes-MacBook-Pro.local>
2020-07-21 19:16:37 -0700

    updated: signin and styles

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
+import SingInandSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
       <Route exact path= '/' component={HomePage} />
       <Route path= '/shop' component={ShopPage} />
+      <Route path= '/signin' component={SingInandSignUpPage} />
       </Switch>
     </div>
   );
---
+++ crwn-clothing/src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SingIn from '../../components/sign-in/sign-in.component'
+import './sign-in-and-sing-up.styles.scss';
+
+const SingInandSignUpPage= () => (
+  <div className='sign-in-and-sing-up'>
+    <SingIn/>
+  </div>
+)
+
+export default SingInandSignUpPage;
